98038d4f93d85a483ef4ede69986db17280e653c
谢灿 <1309382832@qq.com>
2019-08-05 01:37:06 +0800

    xiecan

---
+++ 作业/xiecan/task4/style/task-registe-js.js
@@ -0,0 +1,10 @@
+function fnRegist() {
+    var oPass = document.getElementById("upass").value;
+    var oPass1 = document.getElementById("upass1").value;
+    if (oPass != oPass1) {
+        document.getElementById("error_box").innerHTML = "两次密码输入不一致";
+    }
+    if (oPass == oPass1) {
+        window.location="task4-log.html";
+    }
+}
